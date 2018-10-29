5d7e6f77a78b3fc1b3891f52cb44a56fa99a8364
Sambreezy <tagsamie@gmail.com>
2018-10-29 18:16:57 +0100

    updated project

--- index.php
+++ index.php
@@ -32,6 +32,17 @@
         <div class="navbar-header">
           <h1 style="color:wheat;"><b>UgarSoft</b></h1>
         </div>
+        <ul style="margin-left: -85px;" class="list-inline mb-2">
+          <li class="list-inline-item">
+            <a style="margin-left:20px; font-size: 1.4em;" href="#">Home</a>
+          </li>
+          <li class="list-inline-item">
+            <a style="margin-left:55px; font-size: 1.4em;" href="#">About</a>
+          </li>
+          <li class="list-inline-item">
+            <a style="margin-left:55px; font-size: 1.4em;" href="#">Contact</a>
+          </li>
+        </ul>
         <form class="form-inline" action="#">
           <div class="form-group">
             <input style= "margin-right:20px;" type="email" class="form-control" id="email" placeholder="E-mail" name="email">
@@ -44,6 +55,7 @@
       </div>
     </nav>
 
+
     <!-- Masthead -->
     <header class="masthead">
       <div class="overlay"></div>
@@ -54,25 +66,27 @@
             <h1 class="mb-5" style="color: white; text-align: right;">Document it now!</h1>
           </div>
           <div class="col-xl-4 mx-auto">
-            <form action="#">
+            <div class="embed" style="border-radius: 15px;">
+            <form class="embed1" action="#">
               <div class="form-group">
-                <label class="text-black" for="name"><b>Name:</b></label>
+                <label class="text-black" for="name"></label>
                 <input type="name" class="form-control" id="name" placeholder="Enter name" name="name">
               </div>
               <div class="form-group">
-                <label class="text-black" for="pwd"><b>E-mail:</b></label>
+                <label class="text-black" for="email"><b></label>
                 <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
               </div>
               <div class="form-group">
-                <label class="text-black" for="phone"><b>Phone Number:</b></label>
+                <label class="text-black" for="phone"></label>
                 <input type="phone" class="form-control" id="phone" placeholder="Enter phone number" name="phone">
               </div>
               <div>
-                <label class="text-black" for="pwd"><b>Password:</b></label>
+                <label class="text-black" for="pwd"></label>
                 <input type="pwd" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
               </div><br>
               <button type="submit" class="btn btn-default"><b>Sign Up</b></button>
-            </form><br>
+            </form><br><br>
+            </div><br>
             <div class="container">
               <div class="row">
                 <div class="col-xl-2 mr-0">
@@ -88,19 +102,19 @@
       </div>
     </header>
 
-
-    <div class="container-fluid" style="padding-top: 0px;background-color:#f7f8fa; height:1000px; width:100%">
+ 
+    <div class="container-fluid" style="padding-top: 0px;background-color:#f7f8fa; height:450px; width:100;"><br><br><br>
       <div class="row" >
           <div class="col-md-3" >
               <div id="listdiv">
                   <ul class="list-group" id="list">
                       <h2 id="secondline" class="list-group-item" style="font-weight: bold; margin-top:0px; font-family: 'Times New Roman', Times, serif; background-color:#242438;color:wheat; text-align:center">GAMING</h2>
-                      <li class="list-group-item"><i class="fa fa-thick "></i>Min Deposit - $2000</li>
-                      <li class="list-group-item">Max Deposit - $19999</li>
-                      <li class="list-group-item">3% referral commision</li>
-                      <li class="list-group-item">Capital Protection</li>
-                      <li class="list-group-item">Instant witdrawal</li>
-                      <h5 class="list-group-item" style="background-color:wheat; height:60px; text-align: center; margin-top:0px; color:#242438">BUY NOW</h5>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Min Deposit - $2000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Max Deposit - $19999</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> 3% referral commision</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Capital Protection</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Instant witdrawal</li>
+                      <h5 class="list-group-item" style="background-color:wheat; height:60px; text-align: center; margin-top:0px; color:#242438">JOIN US NOW</h5>
                   </ul>
              </div>
           </div>
@@ -109,12 +123,12 @@
               <div id="listdiv">
                   <ul class="list-group" id="list">
                        <h2 class="list-group-item" style="font-weight: bold; margin-top:0px; font-family: 'Times New Roman', Times, serif; background-color:#242438;color:wheat; text-align:center">WEBSITES</h2>
-                      <li class="list-group-item">Min Deposit - $2000</li>
-                      <li class="list-group-item">Max Deposit - $19999</li>
-                      <li class="list-group-item">3% referral commision</li>
-                      <li class="list-group-item">Capital Protection</li>
-                      <li class="list-group-item">Instant witdrawal</li>
-                      <h5 class="list-group-item" style="background-color:wheat; height:60px; text-align: center; margin-top:0px; color:#242438">BUY NOW</h5>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Min Deposit - $2000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Max Deposit - $19999</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> 3% referral commision</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Capital Protection</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Instant witdrawal</li>
+                      <h5 class="list-group-item" style="background-color:wheat; height:60px; text-align: center; margin-top:0px; color:#242438">JOIN US NOW</h5>
                   </ul>
               </div>
           </div>
@@ -122,12 +136,12 @@
               <div id="listdiv">
                   <ul class="list-group" id="list">
                       <h2 class="list-group-item" style="font-weight: bold; margin-top:0px; font-family: 'Times New Roman', Times, serif; background-color:#242438;color:wheat; text-align:center">OGWUGO</h2>
-                      <li class="list-group-item">Min Deposit - $50000</li>
-                      <li class="list-group-item">Max Deposit - $100000</li>
-                      <li class="list-group-item">3% referral commision</li>
-                      <li class="list-group-item">Capital Protection</li>
-                      <li class="list-group-item">Instant witdrawal</li>
-                      <h5 class="list-group-item" style="background-color:wheat; height:60px; margin-top:0px; text-align: center; color:#242438">BUY NOW</h5>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Min Deposit - $50000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Max Deposit - $100000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> 3% referral commision</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Capital Protection</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Instant witdrawal</li>
+                      <h5 class="list-group-item" style="background-color:wheat; height:60px; margin-top:0px; text-align: center; color:#242438">JOIN US NOW</h5>
                   </ul>
               </div>
           </div>
@@ -135,31 +149,35 @@
               <div id="listdiv">
                   <ul class="list-group" id="list">
                      <h2 class="list-group-item" style="font-weight: bold; margin-top:0px; font-family: 'Times New Roman', Times, serif; background-color:#242438;color:wheat; text-align:center">APPS</h2>
-                      <li class="list-group-item">Min Deposit - $20000</li>
-                      <li class="list-group-item">Max Deposit - $100000</li>
-                      <li class="list-group-item">Capital Protection</li>
-                      <li class="list-group-item">Instant witdrawal</li>
-                      <li class="list-group-item">Instant witdrawal</li>
-                      <h5 class="list-group-item" style="background-color:wheat;height:60px;text-align: center; margin-top:0px; color:#242438">BUY NOW</h5>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Min Deposit - $20000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Max Deposit - $100000</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Capital Protection</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Instant witdrawal</li>
+                      <li class="list-group-item"><i class="fa fa-check"></i> Instant witdrawal</li>
+                      <h5 class="list-group-item" style="background-color:wheat;height:60px;text-align: center; margin-top:0px; color:#242438">JOIN US NOW</h5>
                   </ul>
               </div> 
           </div>
       </div>
-
+    </div>
 
     
-    <div class="container-fluid" style="padding-top: 8.5%; padding-bottom: 3.5%; padding-left:0px; padding-right:0px; background-color: #f7f8fa;">
-      <div class="container">
+    <div class="container-fluid" style="padding-top: 5.5%; padding-bottom: 3.5%; background-color:#f7f8fa;"><hr><br><br>
+      <div class="container-fluid">
         <div class="row">
-          <div class="col-xl-12">
-            <img src="assets/img/ogwugo.jpg"  width="100%" height="450px">
+          <div class="col-xl-7">
+            <img src="assets/img/ogwugo.jpg" style="border-radius: 15px;">
+          </div>
+          <div class="col-xl-5">
+            <h2>About Us</h2>
+            <p>We offer the best online customer-friendly services. With our diligent staffs and personnels who work tirelessly to match the everyday needs of our dear customers, a team of well-equipped programmers who create server-friendly and proper sites using clean codes. We engage in facets such as online gaming, food delivery, online stores and so much more. We promise to provide top-notch service to our esteemed customers, both present and future.</p>
           </div>
         </div>
       </div>
     </div>
      
-    <footer class="footer" style="background-color: #343a40; height: 100px;">
-      <div class="container">
+    <footer class="footer" style="background-color: #343a40; height: 120px;">
+      <div class="container-fluid">
         <div class="row">
           <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
             <ul class="list-inline mb-2">
@@ -185,24 +203,25 @@
             <ul class="list-inline mb-0">
               <li class="list-inline-item mr-3">
                 <a href="#">
-                  <i class="fab fa-facebook fa-2x fa-fw"></i>
+                  <i class="fab fa-facebook fa-2x fa-fw" style="color: #49649f";></i>
                 </a>
               </li>
               <li class="list-inline-item mr-3">
                 <a href="#">
-                  <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+                  <i class="fab fa-twitter-square fa-2x fa-fw" style="color: #17a2b8";></i>
                 </a>
               </li>
               <li class="list-inline-item">
                 <a href="#">
-                  <i class="fab fa-instagram fa-2x fa-fw"></i>
+                  <i class="fab fa-instagram fa-2x fa-fw" style="color:#8a3ab9";></i>
                 </a>
               </li>
             </ul>
           </div>
         </div>
-      </div>
+      </div>  
     </footer>
+
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
